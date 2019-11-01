a859a44bcb3fbd3d11d3a0b9eddad44179fd6e34
tylanvu <57039418+tylanvu@users.noreply.github.com>
2019-11-01 16:15:38 +0100

    Update main.c

--- code/RFID/main.c
+++ code/RFID/main.c
@@ -3,21 +3,23 @@
 
 int main()
 {
-    int tab[4]={12,20,20,12}; // Données qu'on reçoit du RFID
-    int tab2[50];
+    int tab[4]={12,20,20,12}; // Données qu'on reçoit du RFID (sans les 0)
+    int tab2[50]; // Données qui vont contenir les données du RFID (avec les 0)
     char chaine[50]; // Tableau qui va contenir la chaine de caractère
-    int i,k=0;// i=indice de la case k= est un indice qui s'incrémente
+    int i,k=0; // i=indice de la case k= est un indice qui s'incrémente
 
     for(i = 0; i < 4; ++i){ // On regarde case par case jusqu'à Byte.size
       if(tab[i] < 0x10){ // Si le nombre de la case est supérieur à 10
         printf("0"); // On met un 0 avant
         printf("%X",tab[i]); // Puis on écrit le nombre en hexa
+          
         tab2[k]=0; // on met un zéro avant d'écrire le chiffre en hexa
         tab2[k+1]=tab[i]; // on met le chiffre en hexa après la case corresponant au zéro
         k=k+2; // incrément l'indice du 2ème tableau 
       }
       else{
         printf("%X",tab[i]); // Sinon on écrit directement le nombre à 2 chiffre
+          
         tab2[k]=tab[i]; // on écrit le nombre dans la correspondante à l'indice i 
         k=k+1; // incrémente l'indice du 2ème tableau
       }
