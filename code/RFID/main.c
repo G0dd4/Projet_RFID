0f8caf29185c9f0ebab502a54c976b7a376ce990
tylanvu <57039418+tylanvu@users.noreply.github.com>
2019-11-01 16:03:37 +0100

    Update main.c

--- code/RFID/main.c
+++ code/RFID/main.c
@@ -3,23 +3,29 @@
 
 int main()
 {
-    int tab[4]={2,18,0,88}; // Données qu'on reçoit du RFID
+    int tab[4]={12,20,20,12}; // Données qu'on reçoit du RFID
     int tab2[50];
-    char chaine[50]; // Tableau qui va contenir la chaine de caractères
-    int i;
+    char chaine[50]; // Tableau qui va contenir la chaine de caractère
+    int i,k=0;
 
     for(i = 0; i < 4; ++i){ // On regarde case par case jusqu'à Byte.size
       if(tab[i] < 0x10){ // Si le nombre de la case est supérieur à 10
         printf("0"); // On met un 0 avant
         printf("%X",tab[i]); // Puis on écrit le nombre en hexa
+        tab2[k]=0;
+        tab2[k+1]=tab[i];
+        k=k+2;
       }
       else{
-        printf("%X",tab[i]); // Sinon on écrit directement le nombre à 2 chiffres
+        printf("%X",tab[i]); // Sinon on écrit directement le nombre à 2 chiffre
+        tab2[k]=tab[i];
+        k=k+1;
       }
     }
 
-    sprintf(chaine,"\n%x%x%x%x",tab[0],tab[1],tab[2],tab[3]); // Convertion en chaine de caractères
+    sprintf(chaine,"\n%X%X%X%X%X%X",tab2[0],tab2[1],tab2[2],tab2[3],tab2[4],tab2[5],tab2[6]); // Convertion en chaine de caractères
     printf("%s",chaine); // Affichage de la chaine
     return 0;
 }
+
 
