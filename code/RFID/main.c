8d2c0bab571157d11aec8a10d99ff67e6a225ba4
tylanvu <57039418+tylanvu@users.noreply.github.com>
2019-11-05 13:47:34 +0100

    Update main.c

--- code/RFID/main.c
+++ code/RFID/main.c
@@ -17,8 +17,8 @@ void delay(int ms){
 int main(){
   initRC522();
   PCD_Init();
-  int k=0,tab[50];
-  char chaine[50];
+  int k=0, i, tab[10];
+  char chaine[10];
 
   while(1){
     // Look for a card
@@ -27,28 +27,39 @@ int main(){
 
     if( !PICC_ReadCardSerial())
       continue;
-
+    
+    // Initialisation tableau (à mettre dans une fonction)
+    for (i=0; i<10; i++){
+      tab[i]=0;
+      chaine[i]=0;
+    }
     // Print UID
     for(byte i = 0; i < uid.size; ++i){
       if(uid.uidByte[i] < 0x10){
-    printf(" 0");
-    printf("%X",uid.uidByte[i]);
-    
+    printf("0");
+    printf("%X ",uid.uidByte[i]);
+        
+    // Stockage dans le buffer
     tab[k]=0; // on met un zéro avant d'écrire le chiffre en hexa
-    tab[k+1]=uid.Byte[i]; // on met le chiffre en hexa après la case corresponant au zéro
+    tab[k+1]=uid.uidByte[i]; // on met le chiffre en hexa après la case corresponant au zéro
     k=k+2; // incrément l'indice du 2ème tableau 
       }
+      
       else{
-    printf(" ");
-    printf("%X", uid.uidByte[i]);
-    
-    tab[k]=uid.Byte[i]; // on écrit le nombre dans la correspondante à l'indice i 
+    // Print UID 
+    printf("%X ", uid.uidByte[i]);
+        
+    // Stockage dans le buffer
+    tab[k]=uid.uidByte[i]; // on écrit le nombre dans la correspondante à l'indice i 
     k=k+1; // incrémente l'indice du 2ème tableau
       }
 
     }
-    sprintf(chaine,"\n%X%X%X%X%X%X%X",tab[0],tab[1],tab[2],tab[3],tab[4],tab[5],tab[6],tab[7]); // Convertion en chaine de caractères
-    printf("%s",chaine); // Affichage de la chaine
+    // Création de la chaine de caractère
+    for(i=0;i<k;i++){
+    sprintf(chaine,"%s%X",chaine,tab[i]); 
+    }
+    printf("%s",chaine); // Affichage de la chaine (pour test)
     printf("\n");
     delay(1000);
     k=0;
